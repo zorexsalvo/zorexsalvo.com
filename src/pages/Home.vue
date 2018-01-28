eb9fcb6da0644c5fdc2301ca407d4ce69308e5fa
Zorex Salvo <zorexsalvo@gmail.com>
2018-01-28 19:12:38 +0800

    Design the per-article on homepage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,6 +4,33 @@
       <div class="column is-8 is-offset-2">
 
         <div class="card article">
+
+          <div class="card-content">
+            <div class="media">
+              <div class="media-center">
+                <img src="https://daveden.files.wordpress.com/2013/02/linux_cat.jpg" class="author-image" alt="Placeholder image">
+              </div>
+
+              <div class="media-content has-text-centered">
+                  <p class="title article-title">Zorex Salvo, Junior Developer</p>
+                  <p class="subtitle is-6 article-subtitle">
+                  <a href="#">@zorexsalvo</a> on January 27, 2018
+                  </p>
+              </div>
+            </div>
+            <div class="has-text-centered">
+                <span class="tag is-link">introduction</span>
+                <span class="tag is-link">junior developer</span>
+            </div>
+            <div class="content article-body is-centered">
+              <p class="has-text-centered">This was originally posted on [dev.to](https://dev.to/zorexsalvo/zorex-salvo-junior-developer) as my intro blog on dev.to community.</p>
+            </div>
+
+          </div>
+          <a class="button is-light is-fullwidth">Read More</a>
+        </div>
+
+        <div class="card article">
           <div class="card-content">
 
             <div class="media">
@@ -24,6 +51,7 @@
             </div>
           </div>
         </div>
+
       </div>
     </section>
 
