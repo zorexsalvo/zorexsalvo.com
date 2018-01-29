7106f054f8f8c82c1b2556a233568c0faa099952
Zorex Salvo <zorexsalvo@gmail.com>
2018-01-29 23:15:19 +0800

    Add a sidebar for filtering tags and publish year

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,50 +1,74 @@
 <template>
   <div>
-    <section class="articles">
-      <div class="column is-8 is-offset-2">
 
-          <div class="field">
-              <div class="control has-icons-left has-icons-right">
-                  <input class="input" type="email" placeholder="Search@zorexsalvo.com">
-                  <span class="icon is-left">
-                      <i class="fa fa-search"></i>
-                  </span>
-                  <span class="icon is-right">
-                      <i class="fa fa-check"></i>
-                  </span>
-              </div>
-          </div>
-        <div v-for="post in posts"
-             class="card article">
+    <section class="articles container">
+      <div class="columns">
 
-          <div class="card-content">
-            <div class="media">
-              <div class="media-center">
-                <img v-bind:src="post.cover_image" class="author-image" alt="Placeholder image">
-              </div>
+        <div class="column is-3">
+          <aside class="menu tag-panel">
+            <p class="menu-label">
+            Tags
+            </p>
+            <ul class="menu-list">
+              <li>
+                <ul>
+                  <li><a>introduction</a></li>
+                  <li><a>vue.js</a></li>
+                  <li><a>junior developer</a></li>
+                </ul>
+              </li>
+            </ul>
+            <p class="menu-label">
+            Published
+            </p>
+            <ul class="menu-list">
+              <li>
+                <ul>
+                  <li><a>2018</a></li>
+                  <li><a>2017</a></li>
+                </ul>
+              </li>
+            </ul>
 
-              <div class="media-content has-text-centered">
-                  <p class="title article-title">{{ post.title }}</p>
-                  <p class="subtitle is-6 article-subtitle">
-                  <a href="#">@{{ post.created_by }}</a> on {{ formatDate(post.date_created) }}
-                  </p>
-              </div>
-            </div>
-            <div class="has-text-centered">
-              <span v-for="tag in post.tags"
-                    class="tag is-link">
-                {{ tag }}
-              </span>
-            </div>
-            <div class="content article-body is-centered">
-                <p class="has-text-centered">{{ post.short_description }}</p>
-            </div>
-
-          </div>
-          <a class="button is-light is-fullwidth">
-            Read More
-          </a>
+          </aside>
         </div>
+
+        <div class="column is-8">
+
+          <div v-for="post in posts"
+               class="card article">
+
+            <div class="card-content">
+              <div class="media">
+                <div class="media-center">
+                  <img v-bind:src="post.cover_image" class="author-image" alt="Placeholder image">
+                </div>
+
+                <div class="media-content has-text-centered">
+                    <p class="title article-title">{{ post.title }}</p>
+                    <p class="subtitle is-6 article-subtitle">
+                    <a href="#">@{{ post.created_by }}</a> on {{ formatDate(post.date_created) }}
+                    </p>
+                </div>
+              </div>
+              <div class="has-text-centered">
+                <span v-for="tag in post.tags"
+                      class="tag is-primary is-outlined">
+                  {{ tag }}
+                </span>
+              </div>
+              <div class="content article-body is-centered">
+                  <p class="has-text-centered">{{ post.short_description }}</p>
+              </div>
+
+            </div>
+            <a class="button is-light is-fullwidth">
+              Read Article
+            </a>
+          </div>
+
+        </div>
+
 
       </div>
     </section>
