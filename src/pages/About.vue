e8b2d1e63667579e580689d31416ec57d92b7432
Zorex Salvo <zorexsalvo@gmail.com>
2018-02-21 16:29:14 +0800

    Add info notification that says site is under construction

---
+++ src/pages/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="notification is-info has-text-centered">
+      Site is under construction. Please visit
+      <a href="https://zorexsalvo.com">zorexsalvo.com.</a>
+  </div>
+</template>
