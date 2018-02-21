f2fa056646e0ee99e6b9ea8c1ee08280eaeb94ab
Zorex Salvo <zorexsalvo@gmail.com>
2018-02-21 23:11:34 +0800

    Modify the recent posts section
    Remove the article inside the tile.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -25,38 +25,34 @@
     <section class="section">
       <div class="container">
         <h1 class="title">Recent Posts</h1>
-        <div class="columns is-gapless">
+        <div class="columns is-gapless post">
           <div class="column">
-            <div class="tile is-parent">
-              <article class="tile is-child notification">
-                <p class="title is-size-4">Self-Hosted Git Server with Gogs</p>
-                <figure class="image is-4by3">
-                  <img src="https://bulma.io/images/placeholders/640x480.png">
-                </figure>
-              </article>
-            </div>
+            <br>
+            <article class="tile is-child notification">
+              <p class="title is-size-4">Self-Hosted Git Server with Gogs</p>
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/640x480.png">
+              </figure>
+            </article>
           </div>
 
           <div class="column">
-            <div class="tile is-parent">
-              <article class="tile is-child notification">
-                <p class="title is-size-4">Pet Projects: How to stay motivated doing it?</p>
-                <figure class="image is-4by3">
-                  <img src="https://bulma.io/images/placeholders/640x480.png">
-                </figure>
-              </article>
-            </div>
+            <article class="tile is-child notification">
+              <p class="title is-size-4">Pet Projects: How to stay motivated doing it?</p>
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/640x480.png">
+              </figure>
+            </article>
           </div>
 
           <div class="column">
-            <div class="tile is-parent">
-              <article class="tile is-child notification">
-                <p class="title is-size-4">Vue.js, I choose you!</p>
-                <figure class="image is-4by3">
-                  <img src="https://bulma.io/images/placeholders/640x480.png">
-                </figure>
-              </article>
-            </div>
+            <br>
+            <article class="tile is-child notification">
+              <p class="title is-size-4">Vue.js, I choose you!</p>
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/640x480.png">
+              </figure>
+            </article>
           </div>
 
         </div>
