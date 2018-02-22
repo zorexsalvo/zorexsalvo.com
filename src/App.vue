69d3f589de6add4e4d48f1b04bbe6435d39dc97f
Zorex Salvo <zorexsalvo@gmail.com>
2018-02-22 11:00:49 +0800

    Tighten the container width

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,10 @@ export default {
   @import '/assets/css/blog.css';
   @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
   @import 'https://fonts.googleapis.com/css?family=Open+Sans';
+
+  @media only screen and (min-width: 1024px) {
+    .container {
+      width: 1000px !important;
+    }
+  }
 </style>
