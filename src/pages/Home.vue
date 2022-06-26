d90f1f274f30997f4fe978aa58e172e764d8fc5a
Zorex <zorex@brokerengine.com.au>
2022-06-26 16:49:15 +0800

    Change site description

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,7 +10,7 @@
       <div class="column summary">
         <p class="is-size-5 intro">
         I code in Python/Django/Flask and lately I've been doing javascript also. I'm not a
-        design-guy so I'm usually assigned to do backend stuffs.<br/><br/>I'm in continuous
+        design-guy so I'm usually assigned to do backend stuff.<br/><br/>I'm in continuous
         loop of learning and experimenting new technologies ergo my ever-presence on many tech
         conferences, hackathons, and codecamps.<br/><br/>I do blogs too. I write about tech,
         hackathon, experiences, events, anything random that comes to mind.<br/><br/>
