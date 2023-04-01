d8b6ec0307cf33f09268e369ab5c030789f070a4
Zorex <zorex@brokerengine.com.au>
2023-04-01 08:24:32 +0800

    Fix date_posted

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -3,19 +3,17 @@
     <br>
     <div class="timeline is-centered">
       <template v-for="(post, index) in posts">
-        <header class="timeline-header"
-                v-if="(index > 0 && getYear(posts[index]._createdAt) !=
-                       getYear(posts[index-1]._createdAt))"
-                :key="post.id">
-            <span class="tag is-info is-medium">{{ getYear(post._createdAt) }}</span>
+        <header class="timeline-header" v-if="(index > 0 && getYear(posts[index].date) !=
+          getYear(posts[index - 1].date))" :key="post.id">
+          <span class="tag is-info is-medium">{{ getYear(post.date) }}</span>
         </header>
 
         <div class="timeline-item" :key="post.slug.current">
           <div class="timeline-marker"></div>
           <div class="timeline-content">
-            <p class="heading">{{ formatDate(post._createdAt) }}</p>
+            <p class="heading">{{ formatDate(post.date) }}</p>
             <router-link :to="{ name: 'Page', params: { slug: post.slug.current } }">
-                {{ post.title }}
+              {{ post.title }}
             </router-link>
           </div>
         </div>
@@ -39,7 +37,8 @@ export default {
   },
   computed: {
     posts() {
-      return this.$store.getters.allPosts;
+      const posts = this.$store.getters.allPosts;
+      return posts.sort((a, b) => (new Date(b.date) - new Date(a.date)));
     },
   },
   created() {
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,43 +1,47 @@
 <template>
   <div>
-    <br>
+    <br />
     <div class="columns header">
       <div class="column is-three-fifths is-offset-one-quarter summary">
         <p class="is-size-5 intro">
-        I code in Python/Django/Flask and lately I've been doing javascript also. I'm not a
-        design-guy so I'm usually assigned to do backend stuff.<br/><br/>I'm in continuous
-        loop of learning and experimenting new technologies ergo my ever-presence on many tech
-        conferences, hackathons, and codecamps.<br/><br/>I do blogs too. I write about tech,
-        hackathon, experiences, events, anything random that comes to mind.<br/><br/>
+          I code in Python/Django/Flask and lately I've been doing javascript
+          also. I'm not a design-guy so I'm usually assigned to do backend
+          stuff.<br /><br />I'm in continuous loop of learning and experimenting
+          new technologies ergo my ever-presence on many tech conferences,
+          hackathons, and codecamps.<br /><br />I do blogs too. I write about
+          tech, hackathon, experiences, events, anything random that comes to
+          mind.<br /><br />
         </p>
       </div>
     </div>
-    <hr style="margin-bottom:0">
+    <hr style="margin-bottom:0" />
 
     <section class="section">
       <div class="container">
-        <h1 class="title has-text-centered has-text-left-desktop">Recent Posts</h1>
+        <h1 class="title has-text-centered has-text-left-desktop">
+          Recent Posts
+        </h1>
         <div class="columns is-gapless post">
-          <div class="column"
-               v-for="post in recentPosts"
-               :key="post.id">
+          <div class="column" v-for="post in recentPosts" :key="post.id">
             <article class="tile is-child notification">
               <div class="has-text-centered has-text-left-tablet">
-                  {{ formatDate(post._createdAt) }}
+                {{ formatDate(post.date) }}
               </div>
-              <div class="title is-size-5 has-text-centered has-text-left-tablet">
-                <router-link class="router-title"
-                            :to="{ name: 'Page', params: { slug: post.slug.current }}">
+              <div
+                class="title is-size-5 has-text-centered has-text-left-tablet"
+              >
+                <router-link
+                  class="router-title"
+                  :to="{ name: 'Page', params: { slug: post.slug.current } }"
+                >
                   {{ post.title }}
                 </router-link>
               </div>
             </article>
           </div>
-
         </div>
       </div>
     </section>
-
   </div>
 </template>
 
@@ -53,7 +57,9 @@ export default {
   computed: {
     recentPosts() {
       const posts = this.$store.getters.allPosts;
-      return posts.splice(0, 3);
+      return posts
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+        .splice(0, 3);
     },
   },
   created() {
@@ -68,21 +74,27 @@ export default {
     margin-top: 1rem;
   }
 }
+
 article.tile {
-  background-color: #F0F2F4;
+  background-color: #f0f2f4;
 }
+
 p.title {
   height: 30px;
 }
+
 .router-title {
   text-decoration: none !important;
 }
+
 .cover {
   object-fit: cover;
 }
+
 article {
-  padding: 1.25rem .5rem 1.25rem .5rem;
+  padding: 1.25rem 0.5rem 1.25rem 0.5rem;
 }
+
 @media only screen and (max-width: 1000px) {
   .summary {
     margin-left: 8px;
@@ -93,5 +105,4 @@ article {
   min-height: 55vh;
   align-items: center;
 }
-
 </style>
